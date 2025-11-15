650432d5469bfe619597383ff49ff197473a70bb
thongster <106802301+thongster@users.noreply.github.com>
2025-11-15 14:46:20 +0700

    edit gameboarrd function to keep track of ship name and ship count, check if ship is hit, check if ship is sunk, edited tests

--- src/gameboard.js
+++ src/gameboard.js
@@ -17,6 +17,7 @@ export default class Gameboard {
       ['', '', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', ''],
     ];
+    this.shipCount = 0
   }
 
   setShip(ship, dir, x, y) {
@@ -25,17 +26,20 @@ export default class Gameboard {
     // if horizontal
     if (dir === 'horizontal') {
       for (let i = 0; i < ship.length; i++) {
-        this.grid[x][y] = 1;
+        this.grid[x][y] = ship;
         x = x + 1;
       }
     }
     // if vertical
     if (dir === 'vertical') {
       for (let i = 0; i < ship.length; i++) {
-        this.grid[x][y] = 1;
+        this.grid[x][y] = ship;
         y = y + 1;
       }
     }
+
+    // keep track of ship count
+    this.shipCount++
   }
 
   receiveAttack(x, y) {
@@ -45,9 +49,14 @@ export default class Gameboard {
     if (this.grid[x][y] === '') {
       // if empty
       this.grid[x][y] = 0; // confirmed no ship
-    } else if (this.grid[x][y] === 1) {
+    } else if (this.grid[x][y] != '') {
       // if ship is there
-      this.grid[x][y] = 2; // reveal ship
+      const ship = this.grid[x][y]
+      ship.hit() // add to ship hitCount
+      ship.isSunk() // check if ship is sunk with every hit
+      if (ship.sunkStatus) {
+        this.shipCount-- // subtract from shipCount if ship is sunk
+      }
     }
   }
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -5,20 +5,20 @@ test('Set ship vertical', () => {
   const newShip = new Ship(3);
   const newGameboard = new Gameboard
   newGameboard.setShip(newShip, 'vertical', 3, 3);
-  expect(newGameboard.grid[2][2]).toBe(1);
-  expect(newGameboard.grid[2][3]).toBe(1);
-  expect(newGameboard.grid[2][4]).toBe(1);
+  expect(newGameboard.grid[2][2]).toBe(newShip);
+  expect(newGameboard.grid[2][3]).toBe(newShip);
+  expect(newGameboard.grid[2][4]).toBe(newShip);
 });
 
 test('Set ship horizontal', () => {
   const newShip = new Ship(5);
   const newGameboard = new Gameboard
   newGameboard.setShip(newShip, 'horizontal', 5, 8);
-  expect(newGameboard.grid[4][7]).toBe(1);
-  expect(newGameboard.grid[5][7]).toBe(1);
-  expect(newGameboard.grid[6][7]).toBe(1);
-  expect(newGameboard.grid[7][7]).toBe(1);
-  expect(newGameboard.grid[8][7]).toBe(1);
+  expect(newGameboard.grid[4][7]).toBe(newShip);
+  expect(newGameboard.grid[5][7]).toBe(newShip);
+  expect(newGameboard.grid[6][7]).toBe(newShip);
+  expect(newGameboard.grid[7][7]).toBe(newShip);
+  expect(newGameboard.grid[8][7]).toBe(newShip);
 });
 
 test('Receive attack, empty', () => {
@@ -28,9 +28,13 @@ test('Receive attack, empty', () => {
 });
 
 test('Receive attack, contains ship', () => {
-  const newShip = new Ship(5);
+  const newShip = new Ship(2);
   const newGameboard = new Gameboard
   newGameboard.setShip(newShip, 'horizontal', 5, 8);
+  newGameboard.receiveAttack(5, 8);
   newGameboard.receiveAttack(6, 8);
-  expect(newGameboard.grid[5][7]).toBe(2);
+  expect(newGameboard.grid[4][7]).toBe(newShip);
+  expect(newGameboard.grid[5][7]).toBe(newShip);
+  expect(newShip.hitCount).toBe(2)
+  expect(newShip.sunkStatus).toBe(true)
 });
