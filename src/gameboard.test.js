a4619f21520f7fb0e40335c7ffb65532f2756b93
thongster <106802301+thongster@users.noreply.github.com>
2025-11-15 14:19:28 +0700

    test setship horizontal

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,7 +1,7 @@
 import Gameboard from './gameboard.js';
 import Ship from './ship.js'
 
-test.only('Set ship', () => {
+test('Set ship vertical', () => {
   const newShip = new Ship(3);
   const newGameboard = new Gameboard
   newGameboard.setShip(newShip, 'vertical', 3, 3);
@@ -10,7 +10,24 @@ test.only('Set ship', () => {
   expect(newGameboard.grid[2][4]).toBe(1);
 });
 
+test.only('Set ship horizontal', () => {
+  const newShip = new Ship(5);
+  const newGameboard = new Gameboard
+  newGameboard.setShip(newShip, 'horizontal', 5, 8);
+  expect(newGameboard.grid[4][7]).toBe(1);
+  expect(newGameboard.grid[5][7]).toBe(1);
+  expect(newGameboard.grid[6][7]).toBe(1);
+  expect(newGameboard.grid[7][7]).toBe(1);
+  expect(newGameboard.grid[8][7]).toBe(1);
+});
+
 test('Receive attack, empty', () => {
+  const newGameboard = new Gameboard();
+  newGameboard.receiveAttack(3, 3);
+  expect(newGameboard.grid[2][2]).toBe(0);
+});
+
+test('Receive attack, contains ship', () => {
   const newGameboard = new Gameboard();
   newGameboard.receiveAttack(3, 3);
   expect(newGameboard.grid[2][2]).toBe(0);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './modern-normalize.css';
 import './style.css';
 import Ship from './ship.js';
+import Gameboard from './gameboard.js';
+
+  const newShip = new Ship(5);
+  const newGameboard = new Gameboard
+  newGameboard.setShip(newShip, 'horizontal', 5, 8);
+
+  console.log(newGameboard)
