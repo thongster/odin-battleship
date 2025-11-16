2bbd0e3a605377192af806346dd3b501d36b792e
thongster <106802301+thongster@users.noreply.github.com>
2025-11-16 14:17:45 +0700

    started ui module and game driver module. began html/css layout

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import './modern-normalize.css';
 import './style.css';
 import Ship from './ship.js';
 import Gameboard from './gameboard.js';
+import Player from './player.js';
+import { createGrids } from './ui.js';
 
-// const newShip = new Ship(5);
-// const newGameboard = new Gameboard();
-// newGameboard.setShip(newShip, 'horizontal', 5, 8);
-// newGameboard.receiveAttack(6, 8);
-// newGameboard.receiveAttack(5, 8);
-// newGameboard.receiveAttack(8, 8);
+createGrids();
---
+++ src/ui.js
@@ -0,0 +1,28 @@
+function createGrids() {
+  const playerGrid = document.getElementById('playerGrid');
+  const computerGrid = document.getElementById('computerGrid');
+
+  for (let i = 0; i < 10; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    playerGrid.append(cell);
+    for (let i = 0; i < 10; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      playerGrid.append(cell);
+    }
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    computerGrid.append(cell);
+    for (let i = 0; i < 10; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      computerGrid.append(cell);
+    }
+  }
+}
+
+export { createGrids };
